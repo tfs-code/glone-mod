882dafe643931539adbd56fe92980712ca239bf0
tfs-code <tylerfishersmith@gmail.com>
2025-10-28 14:46:41 +0000

    deleted tests

--- src/main/java/net/mcreator/glone/procedures/Test2Procedure.java
+++
@@ -1,6 +0,0 @@
-package net.mcreator.glone.procedures;
-
-public class Test2Procedure {
-	public static void execute() {
-	}
-}
--- src/main/java/net/mcreator/glone/procedures/Test3Procedure.java
+++
@@ -1,6 +0,0 @@
-package net.mcreator.glone.procedures;
-
-public class Test3Procedure {
-	public static void execute() {
-	}
-}
