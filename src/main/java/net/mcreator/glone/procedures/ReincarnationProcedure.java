3cba711f93abfdca8e5a19b5afd9421a36296c82
Gahzu <george@localhost>
2025-10-28 14:56:18 +0000

    added reincarnation

---
+++ src/main/java/net/mcreator/glone/procedures/ReincarnationProcedure.java
@@ -0,0 +1,53 @@
+package net.mcreator.glone.procedures;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.eventbus.api.Event;
+import net.minecraftforge.event.entity.player.PlayerEvent;
+
+import net.minecraft.world.level.Level;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.network.protocol.game.ClientboundUpdateMobEffectPacket;
+import net.minecraft.network.protocol.game.ClientboundPlayerAbilitiesPacket;
+import net.minecraft.network.protocol.game.ClientboundLevelEventPacket;
+import net.minecraft.network.protocol.game.ClientboundGameEventPacket;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.core.BlockPos;
+
+import javax.annotation.Nullable;
+
+@Mod.EventBusSubscriber
+public class ReincarnationProcedure {
+	@SubscribeEvent
+	public static void onPlayerRespawned(PlayerEvent.PlayerRespawnEvent event) {
+		execute(event, event.getEntity());
+	}
+
+	public static void execute(Entity entity) {
+		execute(null, entity);
+	}
+
+	private static void execute(@Nullable Event event, Entity entity) {
+		if (entity == null)
+			return;
+		if (entity instanceof ServerPlayer _player && !_player.level().isClientSide()) {
+			ResourceKey<Level> destinationType = ResourceKey.create(Registries.DIMENSION, ResourceLocation.parse("glone:after_life"));
+			if (_player.level().dimension() == destinationType)
+				return;
+			ServerLevel nextLevel = _player.server.getLevel(destinationType);
+			if (nextLevel != null) {
+				_player.connection.send(new ClientboundGameEventPacket(ClientboundGameEventPacket.WIN_GAME, 0));
+				_player.teleportTo(nextLevel, _player.getX(), _player.getY(), _player.getZ(), _player.getYRot(), _player.getXRot());
+				_player.connection.send(new ClientboundPlayerAbilitiesPacket(_player.getAbilities()));
+				for (MobEffectInstance _effectinstance : _player.getActiveEffects())
+					_player.connection.send(new ClientboundUpdateMobEffectPacket(_player.getId(), _effectinstance));
+				_player.connection.send(new ClientboundLevelEventPacket(1032, BlockPos.ZERO, 0, false));
+			}
+		}
+	}
+}
